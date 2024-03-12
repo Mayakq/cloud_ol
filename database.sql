0ce6104169c16480c0aee491f98b4b6b48beddff
Efim <122534460+Mayakq@users.noreply.github.com>
2024-03-12 11:22:20 +0400

    database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 create table if not exists "users"
 (
-    id       uuid not null ,
+    id       uuid not null default uuid_generate_v4(),
     login    varchar(128)  not null,
     password varchar(1024) not null,
     jswt     varchar(2048)
