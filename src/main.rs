d008f5d72ccf9fb966bb37a6a5a1ceff82a92c4f
Efim <122534460+Mayakq@users.noreply.github.com>
2024-03-18 14:34:15 +0400

    back

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use actix_web::{App, get, HttpServer, Responder, web};
-use actix_web::web::ServiceConfig;
 use env_logger::Env;
-use shuttle_actix_web::ShuttleActixWeb;
-use sqlx::PgPool;
 use tracing::info;
 use tracing_actix_web::TracingLogger;
 use crate::{database::AppState, endpoints::registration::registration};
@@ -16,13 +13,12 @@ mod endpoints;
 mod middleware;
 mod models;
 pub type Data = web::Data<AppState>;
-#[shuttle_runtime::main]
-async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+#[actix_web::main]
+async fn main() {
     let cfg = cfg::Config::init();
-    //let app_state = web::Data::new(AppState::init(&cfg).await);
+    let app_state = web::Data::new(AppState::init(&cfg).await);
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     info!("Application started ♥");
-    let state = web::Data::new(AppState { pool });
     HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
@@ -33,16 +29,6 @@ async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> ShuttleActixWeb<im
         .unwrap()
         .run()
         .await.expect("TODO: panic message");
-    let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(
-            web::scope("/")
-                .wrap(TracingLogger::default())
-                .app_data(state)
-                .configure(config)
-        );
-    };
-
-    Ok(config.into())
 }
 pub fn config(conf: &mut web::ServiceConfig) {
     let scope = web::scope("/api")
